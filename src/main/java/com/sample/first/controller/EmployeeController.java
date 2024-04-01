fb3f7339936a239f596c27d29adc753cd40919e2
harsh-BBi <harsh.sindhwani@bulkbuyindia.com>
2024-04-01 15:47:26 +0530

    this is test controller

--- src/main/java/com/sample/first/controller/EmployeeController.java
+++ src/main/java/com/sample/first/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.sample.first.controller;
 import com.sample.first.entity.Employee;
 import com.sample.first.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,11 @@ public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> test(){
+        return new ResponseEntity<>("This is my test controller", HttpStatus.OK);
+    }
 
     @PostMapping("/addEmployee")
     public ResponseEntity<Object> addEmployee(@RequestBody Employee employee){
